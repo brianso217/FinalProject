b90640920962d27e5d3d737dd62f552afa7309c1
brianso217 <60930057+brianso217@users.noreply.github.com>
2020-12-06 22:03:14 -0500

    installed CORS

--- restapiback.js
+++ restapiback.js
@@ -1,10 +1,10 @@
 var express= require('express');
-
+var cors= require('cors');
 var app= express();
 
 var api_routes= require('./restapiroutes.js');
 app.use('/api', api_routes);
-
+app.use(cors());
 
 app.use('/demo', express.static('frontend'));
 //Start server
