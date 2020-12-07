9920a368619acfc947c1262038336da6c00796c7
brianso217 <60930057+brianso217@users.noreply.github.com>
2020-12-06 21:58:53 -0500

    reversed changes

--- frontend/restapifront.js
+++ frontend/restapifront.js
@@ -3,25 +3,20 @@
 var updateView = async (button) => {
   if(button.dataset.querytype=="by_coursecode"){
     let queryValue= document.querySelector('#codeQuery').value;
-    Access-Control-Allow-Origin: "https://brianso217.netlify.app/frontend/restapifront.html"
     api= `https://cus1172finalprojectso.herokuapp.com/api/by_coursecode/${queryValue}`;
   }
   if(button.dataset.querytype=="by_undergrad"){
-    Access-Control-Allow-Origin: "https://brianso217.netlify.app/frontend/restapifront.html"
     api=`https://cus1172finalprojectso.herokuapp.com/api/by_courselevel/undergraduate`;
   }
   if(button.dataset.querytype=="by_graduate"){
-    Access-Control-Allow-Origin: "https://brianso217.netlify.app/frontend/restapifront.html"
     api=`https://cus1172finalprojectso.herokuapp.com/api/by_courselevel/graduate`;
   }
   if(button.dataset.querytype=="by_coursetitle"){
     let queryValue= document.querySelector('#titleQuery').value;
-    Access-Control-Allow-Origin: "https://brianso217.netlify.app/frontend/restapifront.html"
     api= `https://cus1172finalprojectso.herokuapp.com/api/by_coursetitle/${queryValue}`;
   }
   if(button.dataset.querytype=="by_courseprofessor"){
     let queryValue= document.querySelector('#professorQuery').value;
-    Access-Control-Allow-Origin: "https://brianso217.netlify.app/frontend/restapifront.html"
     api= `https://cus1172finalprojectso.herokuapp.com/api/by_courseprofessor/${queryValue}`;
   }
   const data= await fetch(api);
