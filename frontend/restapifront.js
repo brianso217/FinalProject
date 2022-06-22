24478133d6531ff0ef996c8a9f99b2af3c58583d
Brian So <brianso422@gmail.com>
2022-06-22 10:47:31 -0400

    edited restapifront.js

--- frontend/restapifront.js
+++ frontend/restapifront.js
@@ -3,21 +3,21 @@
 var updateView = async (button) => {
   if(button.dataset.querytype=="by_coursecode"){
     let queryValue= document.querySelector('#codeQuery').value;
-    api= `http://localhost:3000/api/by_coursecode/${queryValue}`;
+    api= `https://cus1172finalprojectso.herokuapp.com/api/by_coursecode/${queryValue}`;
   }
   if(button.dataset.querytype=="by_undergrad"){
-    api=`http://localhost:3000/api/by_courselevel/undergraduate`;
+    api=`https://cus1172finalprojectso.herokuapp.com/api/by_courselevel/undergraduate`;
   }
   if(button.dataset.querytype=="by_graduate"){
-    api=`http://localhost:3000/api/by_courselevel/graduate`;
+    api=`https://cus1172finalprojectso.herokuapp.com/api/by_courselevel/graduate`;
   }
   if(button.dataset.querytype=="by_coursetitle"){
     let queryValue= document.querySelector('#titleQuery').value;
-    api= `http://localhost:3000/api/by_coursetitle/${queryValue}`;
+    api= `https://cus1172finalprojectso.herokuapp.com/api/by_coursetitle/${queryValue}`;
   }
   if(button.dataset.querytype=="by_courseprofessor"){
     let queryValue= document.querySelector('#professorQuery').value;
-    api= `http://localhost:3000/api/by_courseprofessor/${queryValue}`;
+    api= `https://cus1172finalprojectso.herokuapp.com/api/by_courseprofessor/${queryValue}`;
   }
   const data= await fetch(api);
   const model= await data.json();
